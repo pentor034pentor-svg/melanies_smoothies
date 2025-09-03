485111f085af1892540edabdf4758eae74ca9bdb
pentor034pentor-svg <pentor034pentor@gmail.com>
2025-09-03 15:21:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,5 @@ if ingredients_list:
     st.stop() 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+#st.text(smoothiefroot_response)
+sf_df = st.dataframe(data = smoothiesfroot_response.json(),use_container_width = True)
