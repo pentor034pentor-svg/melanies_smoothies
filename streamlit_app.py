a312c6afb835c2d34e68b823b05f78a6ee4ae3f6
pentor034pentor-svg <pentor034pentor@gmail.com>
2025-09-04 10:52:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,13 @@ st.write("The name of your Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data=my_dataframe,use_container_width = True)
+# st.dataframe(data=my_dataframe,use_container_width = True)
+# st.stop()
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
+
+
 
 # Convert to Python list for multiselect
 fruit_options = [row["FRUIT_NAME"] for row in my_dataframe.collect()]
