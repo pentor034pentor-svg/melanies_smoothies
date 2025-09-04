effedbf937372eacd5dc7ff0536e27ec007b9b6c
pentor034pentor-svg <pentor034pentor@gmail.com>
2025-09-04 10:24:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+from snowflake.snowpark.exceptions import SnowparkSQLException
 # Write directly to the app
 st.title(f":watermelon::strawberry: Customize Your Smoothie!:kiwi_fruit::green_apple: {st.__version__}")
 st.write(
