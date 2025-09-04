8dac0279a372b0cfff830a0ec981a42a03e4045e
pentor034pentor-svg <pentor034pentor@gmail.com>
2025-09-04 10:49:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,9 @@ st.write(
 # Input for smoothie name
 name_on_order = st.text_input("Name of Smoothie:")
 st.write("The name of your Smoothie will be:", name_on_order)
+st.dataframe(data=my_dataframe,use_container_width = True)
+st.stop()
+
 
 # Snowflake session
 cnx = st.connection("snowflake")
