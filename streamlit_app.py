f7a4fd2bd7a915a8a78b1d5c1a8221a1c172add5
pentor034pentor-svg <pentor034pentor@gmail.com>
2025-09-04 09:36:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ if ingredients_list:
     st.stop() 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data = smoothiefroot_response.json(),use_container_width = True
+sf_df = st.dataframe(data = smoothiefroot_response.json(),use_container_width = True)
 
                      
   # ingredients_string= ''
@@ -59,6 +59,7 @@ sf_df = st.dataframe(data = smoothiefroot_response.json(),use_container_width = 
   #   smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
   #   )
   
+
 
 
 
