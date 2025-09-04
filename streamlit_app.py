b4b383862a74928f64d63c7a369d83426c454843
pentor034pentor-svg <pentor034pentor@gmail.com>
2025-09-04 09:48:21 +0700

    Update streamlit_app.py_4

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ''
-    for fruit_chosen in ingredient_list:
+    for fruit_chosen in ingredients_list:
       ingredients_string += fruit_chosen + ' '
       st.subheader(fruit_chosen + 'Nutrition information')
       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
